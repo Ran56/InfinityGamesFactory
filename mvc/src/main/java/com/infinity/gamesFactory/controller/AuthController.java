d06f530d002da61dc0c29b8a73b3d445d2caddaa
Ran1111 <rzhang56@gwu.edu>
2021-02-07 23:23:15 -0500

    add CrossOrigin in auth,userRole for reactjs

--- mvc/src/main/java/com/infinity/gamesFactory/controller/AuthController.java
+++ mvc/src/main/java/com/infinity/gamesFactory/controller/AuthController.java
@@ -12,16 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/auth")
 public class AuthController {
     @Autowired
--- mvc/src/main/java/com/infinity/gamesFactory/controller/UserRoleController.java
+++ mvc/src/main/java/com/infinity/gamesFactory/controller/UserRoleController.java
@@ -6,12 +6,14 @@ import com.infinity.gamesFactory.service.RoleService;
 import com.infinity.gamesFactory.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserRoleController {
 
     @Autowired
